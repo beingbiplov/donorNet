1706d92d7d6e01039593ad30cab26da37c5b176e
beingbiplov <beingbiplov@gmail.com>
2020-04-04 18:13:46 +0545

    index page design

--- core/urls.py
+++ core/urls.py
@@ -3,4 +3,16 @@ from .views import index
 
 urlpatterns = [
     path('', index, name='index'),
+
+    path('donr-register', index, name='regdonor'),
+    path('searchresult', index, name='searchresult'),
+    path('donorlist', index, name='donorlist'),
+ #   path('add-record', views.addrecord, name='addrecord'),
+    path('contact', index, name='contact'),
+    path('donor-confirmation', index, name='donorconf'),
+    path('pending-donors', index, name='pendingdonors'),
+    path('donor/<int:pk>/update', index, name='donor-update'),
+    path('blood-register', index, name='bloodregister'),
+    path('search-blood', index, name='searchblood'),
+    path('search-donor',index, name='searchdonor'),
 ]
